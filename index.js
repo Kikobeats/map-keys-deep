2952b337cc714c8b8dc3d4fc234dea41924f47ce
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-04 11:06:05 +0200

    fix: typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mapKeysDeep = (obj, fn) =>
   Array.isArray(obj)
     ? obj.map(item => mapKeysDeep(item, fn))
     : isPlainObject(obj)
-    ? mapValues(mapKeys(obj, fn), value => mapKeysDeep(value, fn))
-    : obj
+      ? mapValues(mapKeys(obj, fn), value => mapKeysDeep(value, fn))
+      : obj
 
 module.exports = mapKeysDeep
